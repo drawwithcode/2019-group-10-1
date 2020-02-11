1fe6f0e33cbf90f81cc9aea247f39604a3e8e19d
matildeteani <matilde.teani@libero.it>
2020-02-11 22:46:23 +0100

    new whatsapp link

--- sketch.js
+++ sketch.js
@@ -63,7 +63,7 @@ function showButtons() {
   }
 
   this.openWhatsapp = function () {
-    window.location.href = 'whatsapp://send?text=Condividi con i tuoi amici %0Ahttps://elisamanzoni.github.io/photo-mosaic-prova2/';
+    window.location.href = 'whatsapp://send?text=Condividi con i tuoi amici %0Ahttps://drawwithcode.github.io/2019-group-10-1/';
   }
 
   this.openSpotify = function () {
